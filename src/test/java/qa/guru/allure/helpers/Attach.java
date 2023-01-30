2dded209da729af27650596989abfb781df842de
Aleksey Volosovich <aleksey.volosovich@quadcode.com>
2023-01-30 18:43:53 +0200

    add imports in Attach class

--- src/test/java/qa/guru/allure/helpers/Attach.java
+++ src/test/java/qa/guru/allure/helpers/Attach.java
@@ -5,8 +5,11 @@ import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
+import static com.codeborne.selenide.Selenide.sessionId;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static org.openqa.selenium.logging.LogType.BROWSER;
 
@@ -42,7 +45,6 @@ public class Attach {
 
     public static URL getVideoUrl() {
         String videoUrl = "https://selenoid.autotests.cloud/video/" + sessionId() + ".mp4";
-//        System.out.println(sessionId());
         try {
             return new URL(videoUrl);
         } catch (MalformedURLException e) {
